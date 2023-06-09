c9e9212c4e429e1c3f5c6ee6c8952ded66d84dce
DenisSvistoplasov <deniska_roms@mail.ru>
2023-06-09 20:03:21 +0400

    zoom on input fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -18,7 +18,7 @@ export default function App({ Component, pageProps }: AppProps) {
       <Head>
         <meta charSet="UTF-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
         <title>Messenger</title>
       </Head>
       <Provider store={store}>
